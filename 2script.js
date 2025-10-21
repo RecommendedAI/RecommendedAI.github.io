9d73350120565bcdaf8cecb76c5a828345512a61
SuggestedTools <159648314+SuggestedTools@users.noreply.github.com>
2025-10-21 23:19:17 +0100

    Create 2script.js

---
+++ 2script.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-TZS7N28792"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-TZS7N28792');
+</script>
